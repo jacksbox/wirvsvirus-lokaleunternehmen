a6d404a3a3b6bf46de4c92e73502531f7feb54b1
jacomo <github@jacksbox.de>
2020-03-22 18:02:29 +0100

    add video

--- src/functionalComponents/Home/index.js
+++ src/functionalComponents/Home/index.js
@@ -12,6 +12,7 @@ const Home = () => (
       }}>
       <div style={{ padding: '40px', textAlign: 'center' }}>
         <img src={logo} alt="logo" style={{ width: '200px', maxWidth: "80%"}} />
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/EW529J86Mpk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         <h4>
           Mit <strong>Bleib Lokal!</strong> entwickeln wir eine Lösung,<br />
           um die Läden um die Ecke in dieser schwierigen Zeit zu unterstützen.
