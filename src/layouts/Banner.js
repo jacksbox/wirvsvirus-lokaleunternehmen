03c32c19ed62e3570c4d762738f88f196a773dbf
jacomo <github@jacksbox.de>
2020-03-23 20:02:04 +0100

    imporve banner

--- src/layouts/Banner.js
+++ src/layouts/Banner.js
@@ -10,20 +10,11 @@ const Banner = () => (
         backgroundColor: "rgba(125, 171, 162, .25)"
       }}
     >
-      <strong>Achtung: Dies ist bisher leider nur eine Demo-Applikation</strong>
-      <br />
-      Es werden noch keine Anfragen verschickt oder Unternehmen eingetragen.
-      Unterstützt uns mit Likes und Kommentaren zu unserem{" "}
-      <a href="https://www.youtube.com/watch?v=EW529J86Mpk&feature=youtu.be">
-        <strong>Intro Video</strong>
-      </a>
-      <br />
-      Sie möchten mit ihrem Unternehmendabei sein sobald es losgeht? Dann tragen
-      Sie sich{" "}
-      <a href="https://docs.google.com/forms/d/e/1FAIpQLScaCVS7ej3dcFbiOBTck3wcjDyytzvIFzvHCStJt8Ir-9u7vQ/viewform?usp=sf_link">
-        <strong>hier</strong>
-      </a>{" "}
-      ein.
+      <h4 style={{ margin: 0, fontWeight: 'bold' }}>Demo Applikation</h4>
+      Das Angebot befindet sich im Aufbau, bitte nutzen Sie zur Registrierung dieses <a href="https://docs.google.com/forms/d/e/1FAIpQLScaCVS7ej3dcFbiOBTck3wcjDyytzvIFzvHCStJt8Ir-9u7vQ/viewform?usp=sf_link">
+        <strong>Formular</strong>
+      </a>.<br />
+      Anfragen werden derzeit noch nicht an Unternehmen übermittelt.
     </div>
   </>
 );
