b0bda878f69e793097d9d79c50309320608ecb4c
jacomo <github@jacksbox.de>
2020-03-24 16:51:50 +0100

    removed unused imports

--- src/container/LoginRegister/index.js
+++ src/container/LoginRegister/index.js
@@ -7,7 +7,6 @@ import GridContainer from "components/Grid/GridContainer.js";
 import Card from "components/Card/Card.js";
 import CardHeader from "components/Card/CardHeader.js";
 import CardBody from "components/Card/CardBody.js";
-import CardFooter from "components/Card/CardFooter.js";
 
 import SuccessMessage from "./SuccessMessage";
 
