32923bde7b3b65ae1846d118a0936305d4a0eb49
jacomo <github@jacksbox.de>
2020-03-30 00:31:39 +0200

    migrate SuccessMessage to own components

--- src/functionalComponents/LoginRegister/SuccessMessage.js
+++ src/functionalComponents/LoginRegister/SuccessMessage.js
@@ -1,13 +1,12 @@
 import React from "react";
-import Success from "components/Typography/Success.js";
 
 const SuccessMessage = () => (
-  <Success>
+  <>
     <h3>Ihr Unternehmen wurde registriert!</h3>
     <h5>
       Ab jetzt können Ihre Kunden Sie auf unserer Karte finde
     </h5>
-  </Success>
+  </>
 )
 
 export default SuccessMessage
