a0eb1b70ff4302b6c40898437ee10894cb72253e
jacomo <github@jacksbox.de>
2020-03-29 15:41:53 +0200

    add select option to Input component

--- src/functionalComponents/Input/index.js
+++ src/functionalComponents/Input/index.js
@@ -1,30 +1,41 @@
 import React from "react";
 
 import TextField from "@material-ui/core/TextField";
+import MenuItem from "@material-ui/core/MenuItem";
 
 const Input = ({
   id,
-  defaultValue,
+  defaultValue = null,
+  value,
+  select,
+  options,
   required = false,
   labelText,
-  helperText = 'Bitte ausfüllen',
+  helperText = "Bitte ausfüllen",
   handleChange,
   errors,
-  size = 'small',
+  size = "small",
   fullWidth = false,
   multiline = false,
   rows = null
 }) => {
-
   let error = false;
   if (errors.includes(id)) {
     error = true;
   }
 
+  let valueOrDefault = {};
+  if (value) {
+    valueOrDefault.value = value;
+  } else if (defaultValue) {
+    valueOrDefault.defaultValue = defaultValue;
+  }
+
   return (
     <TextField
       id={id}
-      defaultValue={defaultValue}
+      select={select}
+      {...valueOrDefault}
       required={required}
       label={labelText}
       helperText={error ? helperText : null}
@@ -37,7 +48,15 @@ const Input = ({
       inputProps={{
         name: id
       }}
-    />
+    >
+      {select && options && options.length
+        ? options.map(({ value, label }) => (
+            <MenuItem key={value} value={value}>
+              {label}
+            </MenuItem>
+          ))
+        : null}
+    </TextField>
   );
 };
 
