79b1d23e4ca61f958598223c8ceeb7ce20db53db
jacomo <github@jacksbox.de>
2020-03-30 00:38:31 +0200

    cleaner view for home

--- src/functionalComponents/Home/index.js
+++ src/functionalComponents/Home/index.js
@@ -1,31 +1,35 @@
 import React from "react";
 
+import Paper from "@material-ui/core/Paper";
 import Button from "@material-ui/core/Button";
 
 const Home = () => (
-  <div>
-      <div style={{
-        position: 'absolute',
-        maxWidth: '100%',
-        top: '50px',
-        left: '50%',
-        transform: 'translate(-50%, 0)',
-        background: '#fff',
-        borderRadius: '20px', width: '760px'
-      }}>
-      <div style={{ padding: '40px', textAlign: 'center' }}>
-        {/* <img src={logo} alt="logo" style={{ width: '200px', maxWidth: "80%"}} /> */}
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/EW529J86Mpk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        <h4>
-          Mit <strong>Bleib Lokal!</strong> entwickeln wir eine Lösung,<br />
-          um die Läden um die Ecke in dieser schwierigen Zeit zu unterstützen.
-        </h4>
-        <Button variant="contained" href="/customer/maps" color="primary">
-          Jetzt lokale Unternehmen finden
-        </Button>
-      </div>
-      </div>
-  </div>
-)
+  <Paper
+    style={{
+      maxWidth: "900px",
+      textAlign: "center",
+      padding: "80px 40px",
+      margin: "0 auto"
+    }}
+  >
+    {/* <img src={logo} alt="logo" style={{ width: '200px', maxWidth: "80%"}} /> */}
+    <iframe
+      width="560"
+      height="315"
+      src="https://www.youtube.com/embed/EW529J86Mpk"
+      frameborder="0"
+      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    ></iframe>
+    <h4>
+      Mit <strong>Bleib Lokal!</strong> entwickeln wir eine Lösung,
+      <br />
+      um die Läden um die Ecke in dieser schwierigen Zeit zu unterstützen.
+    </h4>
+    <Button variant="contained" href="/customer/maps" color="primary">
+      Jetzt lokale Unternehmen finden
+    </Button>
+  </Paper>
+);
 
-export default Home
+export default Home;
