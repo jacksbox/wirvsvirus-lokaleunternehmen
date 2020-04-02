263865e09a21291c5a4338a646de9fa5638b8a65
jacomo <github@jacksbox.de>
2020-04-02 20:11:33 +0200

    better formating for SuccessMessage

--- src/components/SignUp/SuccessMessage.js
+++ src/components/SignUp/SuccessMessage.js
@@ -4,7 +4,7 @@ import Typography from '@material-ui/core/Typography'
 
 const SuccessMessage = () => (
   <>
-    <Typography variant="h3">Ihr Unternehmen wurde registriert!</Typography>
+    <Typography variant="h5">Ihr Unternehmen wurde registriert!</Typography>
     <Typography variant="body1">
       Sobald Ihr Eintrag von uns verifiziert wurde, können Kunden Sie auf unserer Karte finden.
     </Typography>
