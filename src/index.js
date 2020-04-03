a7137931e42984e6a7f8ccfa7b2e1064b1ac335d
jacomo <github@jacksbox.de>
2020-04-03 19:53:58 +0200

    add sentry

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import * as Sentry from '@sentry/browser';
+
 import App from './App.js'
+
+Sentry.init({ dsn: 'https://bf1f239fbacb4e38a352eb2bc1f49828@sentry.io/5187973' });
 
 ReactDOM.render(
   <App />,
