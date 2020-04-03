8a3e7478ac475c1543267f2826754902b6a26df5
jacomo <github@jacksbox.de>
2020-04-03 19:11:04 +0200

    now with correct mail adress

--- src/components/SignUp/SuccessMessage.js
+++ src/components/SignUp/SuccessMessage.js
@@ -7,7 +7,7 @@ const SuccessMessage = () => (
     <Typography variant="h5">Ihr Unternehmen wurde registriert!</Typography>
     <Typography variant="body1">
       Wir benachrichtigen Sie, sobald Ihr Geschäft auf der Karte für Ihre Kunden sichtbar wird.<br />
-      Sie haben weitere Fragen zu Ihrer Registrierung? Dann schreiben Sie uns bitte eine Mail an <a href="mailto:bleiblokal@gmail.de">bleiblokal@gmail.de</a>.
+      Sie haben weitere Fragen zu Ihrer Registrierung? Dann schreiben Sie uns bitte eine Mail an <a href="mailto:bleib.lokal2020@gmail.com">bleib.lokal2020@gmail.com</a>.
     </Typography>
   </>
 )
