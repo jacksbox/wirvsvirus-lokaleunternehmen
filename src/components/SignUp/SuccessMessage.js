f3007ee64d18097363450ae804e78008f5fb030e
jacomo <github@jacksbox.de>
2020-04-05 22:53:32 +0200

    closes #35 add space before eMail in SignUp Success Message

--- src/components/SignUp/SuccessMessage.js
+++ src/components/SignUp/SuccessMessage.js
@@ -7,7 +7,7 @@ const SuccessMessage = () => (
     <Typography variant="h5">Ihr Unternehmen wurde registriert!</Typography>
     <Typography variant="body1">
       Sobald Ihr Eintrag von uns verifiziert wurde, können Kunden Sie auf unserer Karte finden. Wir benachrichtigen Sie, sobald Ihr Geschäft auf der Karte für Ihre Kunden sichtbar wird.<br />
-      Sie haben weitere Fragen zu Ihrer Registrierung? Dann schreiben Sie uns gerne eine Mail an<a href="mailto:bleib.lokal2020@gmail.com">bleib.lokal2020@gmail.com</a>.
+      Sie haben weitere Fragen zu Ihrer Registrierung? Dann schreiben Sie uns gerne eine Mail an <a href="mailto:bleib.lokal2020@gmail.com">bleib.lokal2020@gmail.com</a>.
     </Typography>
   </>
 )
