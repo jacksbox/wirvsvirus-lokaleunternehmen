b8711e200978a829e1ae0204cfd428ce8e92c1fa
jacomo <github@jacksbox.de>
2020-04-06 20:03:09 +0200

    add from/to times

--- src/components/BusinessHours/index.js
+++ src/components/BusinessHours/index.js
@@ -6,7 +6,7 @@ import Button from "@material-ui/core/Button";
 
 import Input from "components/Input";
 
-import update from 'immutability-helper';
+import update from "immutability-helper";
 
 const daysOptions = [
   { label: "Montag", value: "A_1" },
@@ -30,102 +30,155 @@ const minutesOptions = [
   { label: "45", value: 45 },
 ];
 
-const renderItem = ({ item, index, handleChange, handleDelete, errors, numItems }) => {
-  const { d, h, m } = item
+const renderItem = ({
+  item,
+  index,
+  handleChange,
+  handleDelete,
+  errors,
+  numItems,
+}) => {
+  const { day, start_hours, start_minutes, end_hours, end_minutes } = item;
   return (
-  <Grid item xs={12} container spacing={2}>
-    <Grid item xs={2}>
-
+    <Grid item xs={12} container spacing={2}>
+      <Grid item xs={12} md={2}>
+        <Input
+          id={`day-${index}`}
+          select
+          value={day}
+          options={daysOptions}
+          required
+          labelText="Tag"
+          helperText="Zeiten dürfen Sich nicht überlappen"
+          handleChange={handleChange(index)}
+          errors={errors}
+          variant="outlined"
+          fullWidth
+        />
+      </Grid>
+      <Grid item xs={12} sm={8} container spacing={2}>
+        <Grid item xs={12} md={6} container spacing={2}>
+          <Grid item xs={2}>
+            von
+          </Grid>
+          <Grid item xs={5}>
+            <Input
+              id={`start_hours-${index}`}
+              select
+              value={start_hours}
+              options={hoursOptions}
+              required
+              labelText="Stunde"
+              helperText=""
+              handleChange={handleChange(index)}
+              errors={errors}
+              variant="outlined"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={5}>
+            <Input
+              id={`start_minutes-${index}`}
+              select
+              value={start_minutes}
+              options={minutesOptions}
+              required
+              labelText="Minuten"
+              helperText=""
+              handleChange={handleChange(index)}
+              errors={errors}
+              variant="outlined"
+              fullWidth
+            />
+          </Grid>
+        </Grid>
+        <Grid item xs={12} md={6} container spacing={2}>
+          <Grid item xs={2}>
+            bis
+          </Grid>
+          <Grid item xs={5}>
+            <Input
+              id={`end_hours-${index}`}
+              select
+              value={end_hours}
+              options={hoursOptions}
+              required
+              labelText="Stunde"
+              helperText=""
+              handleChange={handleChange(index)}
+              errors={errors}
+              variant="outlined"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={5}>
+            <Input
+              id={`end_minutes-${index}`}
+              select
+              value={end_minutes}
+              options={minutesOptions}
+              required
+              labelText="Minuten"
+              helperText=""
+              handleChange={handleChange(index)}
+              errors={errors}
+              variant="outlined"
+              fullWidth
+            />
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item xs={12} sm={2}>
+        <Button onClick={handleDelete(index)} disabled={numItems === 1}>
+          entfernen
+        </Button>
+      </Grid>
     </Grid>
-    <Grid item xs={2}>
-      <Input
-        id={`d_${index}`}
-        select
-        value={d}
-        options={daysOptions}
-        required
-        labelText="Tag"
-        helperText="Zeiten dürfen Sich nicht überlappen"
-        handleChange={handleChange(index)}
-        errors={errors}
-        variant="outlined"
-        fullWidth
-      />
-    </Grid>
-    <Grid item xs={1}>
-      <Input
-        id={`h_${index}`}
-        select
-        value={h}
-        options={hoursOptions}
-        required
-        labelText="Stunde"
-        helperText=""
-        handleChange={handleChange(index)}
-        errors={errors}
-        variant="outlined"
-        fullWidth
-      />
-    </Grid>
-    <Grid item xs={1}>
-      <Input
-        id={`m_${index}`}
-        select
-        value={m}
-        options={minutesOptions}
-        required
-        labelText="Minuten"
-        helperText=""
-        handleChange={handleChange(index)}
-        errors={errors}
-        variant="outlined"
-        fullWidth
-      />
-    </Grid>
-    <Grid item xs={2}>
-      <Button onClick={handleDelete(index)} disabled={numItems === 1}>entfernen</Button>
-    </Grid>
-  </Grid>
-)};
+  );
+};
 
 const baseItem = {
-  d: "A_1",
-  h: 12,
-  m: 0
-}
+  day: "A_1",
+  start_hours: 12,
+  start_minutes: 0,
+  end_hours: 18,
+  end_minutes: 0,
+};
 
 const BusinessHours = ({ updateBusinessHours, errors = [] }) => {
   const [items, setItems] = useState([{ ...baseItem }]);
 
   useEffect(() => {
-    updateBusinessHours(items)
-  }, [])
+    updateBusinessHours(items);
+  }, []);
 
   const handleAdd = () => {
     if (items.length >= 21) {
-      return
+      return;
     }
-    const newItems = update(items, { $push: [{ ...baseItem }] })
-    setItems(newItems)
-  }
+    const newItems = update(items, { $push: [{ ...baseItem }] });
+    setItems(newItems);
+    console.log(newItems)
+  };
 
-  const handleChange = index => event => {
+  const handleChange = (index) => (event) => {
     const newItem = {
       ...items[index],
-      [event.target.name.charAt(0)]: event.target.value
-    }
-    const newItems = update(items, { [index]: { $set: newItem } })
-    setItems(newItems)
-    updateBusinessHours(newItems)
-  }
+      [event.target.name.split("-")[0]]: event.target.value,
+    };
+    const newItems = update(items, { [index]: { $set: newItem } });
+    setItems(newItems);
+    updateBusinessHours(newItems);
+    console.log(newItems)
+  };
 
-  const handleDelete = index => () => {
+  const handleDelete = (index) => () => {
     if (items.length === 1) {
-      return
+      return;
     }
     const newItems = update(items, { $splice: [[index, 1]] });
-    setItems(newItems)
-  }
+    setItems(newItems);
+  };
 
   return (
     <Grid container spacing={2}>
@@ -133,11 +186,21 @@ const BusinessHours = ({ updateBusinessHours, errors = [] }) => {
         <Typography variant="h6">Öffnungszeiten</Typography>
       </Grid>
       <Grid item xs={12}></Grid>
-      {items.map((item, index) => renderItem({ item, index, handleChange, handleDelete, errors, numItems: items.length }))}
+      {items.map((item, index) =>
+        renderItem({
+          item,
+          index,
+          handleChange,
+          handleDelete,
+          errors,
+          numItems: items.length,
+        })
+      )}
       <Grid item xs={12} container spacing={2}>
-        <Grid item xs={2} />
-        <Grid item xs={3}>
-          <Button onClick={handleAdd} variant="contained">Hinzufügen</Button>
+        <Grid item xs={12} sm={4} md={2}>
+          <Button onClick={handleAdd} variant="contained" fullWidth>
+            Hinzufügen
+          </Button>
         </Grid>
       </Grid>
     </Grid>
